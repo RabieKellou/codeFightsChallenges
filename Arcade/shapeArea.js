04f34baef5196e28d26501bb5f6d004268a5c815
= <=>
2018-05-01 19:18:18 +0100

    Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.

---
+++ Arcade/shapeArea.js
@@ -0,0 +1,18 @@
+function shapeArea(n) {
+    /* area : shapeArea 
+       add: how many polygones we gonna add to the next iterartion
+    */
+    var area = 1,
+        add = 4; // intializing.. 
+
+    for (i = 2; i <= n; i++) {
+        area += add;
+        add = i * 4;
+    }
+    return area
+}
+
+// another perspective:
+// function shapeArea(n) {
+//     return n * n + (n - 1) * (n - 1);
+//   }
