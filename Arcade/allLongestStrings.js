a2f854d164089c2ca655bc7c7425957df734f368
= <=>
2018-05-04 09:08:19 +0100

    Given an array of strings, return another array containing all of its longest strings.

--- Arcade/allLongestStrings.js
+++ Arcade/allLongestStrings.js
@@ -1,3 +1,4 @@
+// Task url: https://codefights.com/arcade/intro/level-3/fzsCQGYbxaEcTr2bL
 function allLongestStrings(inputArray) {
     var sorted = inputArray.sort((a, b) => a.length - b.length);
 
