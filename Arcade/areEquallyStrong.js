8b91662919b60e07fa41f7ad8d6d0ef3b36e3da1
= <=>
2018-12-08 11:57:27 +0000

    Call two arms equally strong if the heaviest weights they each are able to lift are equal...

---
+++ Arcade/areEquallyStrong.js
@@ -0,0 +1,4 @@
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+    return (yourLeft === friendsLeft || yourLeft === friendsRight) &&
+        (yourRight === friendsLeft || yourRight === friendsRight);
+}
